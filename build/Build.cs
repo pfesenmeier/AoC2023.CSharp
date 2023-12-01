4a8038f2c6d8ec41fba8478664909eadab8be353
Paul Fesenmeier <pfesenmeier@gmail.com>
2023-12-01 13:01:13 -0500

    finish templating tool

--- build/Build.cs
+++ build/Build.cs
@@ -7,6 +7,7 @@ using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Utilities.Collections;
+using Serilog;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
@@ -26,14 +27,43 @@ class Build : NukeBuild
     [Parameter("Year")]
     readonly int Year = 2023;
 
+    string ProgramTemplate() => $$"""
+using Solution = System.Func<string, string>;
+
+string projectDir = Environment.SpecialFolder.UserProfile + @"\{{RootDirectory.GetRelativePathTo(DayPath())}}";
+string sampleInput = projectDir + @"\sample.txt"; 
+string puzzleInput = projectDir + @"\input.txt";
+
+Solution Part1 = (string input) => 
+{
+    return "TODO";
+};
+
+Solution Part2 = (string input) => 
+{
+    return "TODO";
+};
+
+Console.WriteLine($"Part 1 Sample");
+Console.WriteLine(Part1(sampleInput));
+Console.WriteLine($"Part 1 Solution");
+Console.WriteLine(Part1(puzzleInput));
+
+Console.WriteLine($"Part 2 Sample");
+Console.WriteLine(Part2(sampleInput));
+Console.WriteLine($"Part 2 Solution");
+Console.WriteLine(Part2(puzzleInput));
+""";
+
     AbsolutePath YearPath() => AdventCalendars / Year.ToString();
+    AbsolutePath DayPath() => YearPath() / Day.ToString();
 
     Target SetDay => _ => _
         .DependsOn(EnsureYearExists)
         .OnlyWhenDynamic(() => Day is null)
         .Executes(() =>
     {
-        var fileNames = YearPath().GetFiles().Select(file => file.Name).Select(name => int.Parse(name));
+        var fileNames = YearPath().GetDirectories().Select(file => file.Name).Select(name => int.Parse(name));
 
         Day = fileNames.Count() is 0 ? 1 : fileNames.Max() + 1;
     });
@@ -41,18 +71,33 @@ class Build : NukeBuild
     Target EnsureYearExists => _ => _
         .Executes(() => (AdventCalendars / Year.ToString()).CreateDirectory());
 
+    Target EnsureDayExists => _ => _
+        .DependsOn(SetDay)
+        .Executes(() => DayPath().CreateDirectory());
 
     Target StartDay => _ => _
-        .DependsOn(SetDay)
+        .DependsOn(EnsureDayExists)
         .Executes(() =>
         {
-            var projectDirectory = AdventCalendars / Year.ToString() / Day.ToString();
-            projectDirectory.CreateDirectory();
+            Log.Debug($"Templating Day {Day}");
 
-            var srcDir = projectDirectory / "src";
-            var testDir = projectDirectory / "test";
+            var srcDir = DayPath() / $"Day{Day}";
+            var testDir = DayPath() / $"Day{Day}.Test";
             DotNet($"new console -o {srcDir}");
             DotNet($"new xunit -o {testDir}");
             DotNet($"add {testDir} reference {srcDir}");
+            DotNet($"sln add {srcDir}");
+            DotNet($"sln add {testDir}");
         });
+
+    Target TemplateProgram => _ => _
+       .TriggeredBy(StartDay)
+       .Executes(() =>
+       {
+           var programPath = DayPath() / "src" / "Program.cs";
+           programPath.WriteAllText(ProgramTemplate());
+
+           (DayPath() / "input.txt").TouchFile();
+           (DayPath() / "sample.txt").TouchFile();
+       });
 }
